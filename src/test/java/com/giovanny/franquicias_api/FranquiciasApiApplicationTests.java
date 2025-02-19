098511bab85e56d675b6196f470fa7e2108e2940
Giovanny Gonzalez <ing.hggw@gmail.com>
2025-02-18 19:14:34 -0500

    instalación de springboot

---
+++ src/test/java/com/giovanny/franquicias_api/FranquiciasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanny.franquicias_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
